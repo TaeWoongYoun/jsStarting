035c10656ee252a97240561ade5e9450e5b4873e
xodnd0507 <24457545yong@gmail.com>
2024-04-15 10:22:00 +0900

    test : Update click event

--- keyboard/index.js
+++ keyboard/index.js
@@ -1,5 +1,22 @@
-document.addEventListener("keydown", function(event){
-    if (event.code === "KeyA"){
-        console.log(1)
+// document.addEventListener("keydown", function(event){
+//     if (event.code === "KeyA"){
+            // console.log("A")
+//     }
+// })
+
+
+// function textInput(a,b){
+//     document.getElementById(a).addEventListener("click", function(){
+//         document.getElementById("textarea").value = b;
+//     })
+// }
+
+function textInput(a,b){
+    if (document.getElementById(a)){
+        document.getElementById(a).addEventListener("click", function(){
+            document.getElementById("textarea").value = b;
+        })
     }
-})
+}
+
+textInput("one", "1")
