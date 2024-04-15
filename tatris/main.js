f382c06192c0fb1f2b11ad5261a43a4310ffbe72
xodnd0507 <24457545yong@gmail.com>
2024-04-15 14:25:59 +0900

    test : Test Block

--- tatris/main.js
+++ tatris/main.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 
 // 상수를 이용해 캔버스의 크기 계산
 ctx.canvas.width = COLS * BLOCK_SIZE;
-ctx.canvas.height = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // 블록의 크기 변경
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
@@ -12,5 +12,9 @@ let board = new Board();
 
 function play(){
     board.reset();
+    let piece = new Piece(ctx);
+    piece.draw();
+
+    board.piece = piece;
     console.table(board.grid);
 }
