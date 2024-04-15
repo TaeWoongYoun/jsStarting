bad180c7737005d5fb54250721749f44c2a095e5
xodnd0507 <24457545yong@gmail.com>
2024-04-15 13:23:46 +0900

    style : Desgin update

---
+++ tatris/main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+// 상수를 이용해 캔버스의 크기 계산
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = COLS * BLOCK_SIZE;
+
+// 블록의 크기 변경
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
