ed41a8e5b9d9fc294cffc2e1d07de06b0b2a98ec
xodnd0507 <24457545yong@gmail.com>
2024-04-22 11:40:13 +0900

    fix : Change code

--- fontSizeUp/index.js
+++ fontSizeUp/index.js
@@ -1,7 +1,3 @@
-// for(var i = 0; i<i+1; i++){
-//     console.log(i);
-// }
-
 document.getElementById('btn').addEventListener("click", function(){
     document.getElementById('text').style.fontSize = (parseInt(window.getComputedStyle(document.getElementById('text'), null).getPropertyValue('font-size')) + 5) + 'px';
 })
