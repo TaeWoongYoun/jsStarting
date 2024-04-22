b138d7281ca0c4b45758f6f1539f81eaf3c52734
xodnd0507 <24457545yong@gmail.com>
2024-04-22 10:54:17 +0900

    test : Fontsize btn

--- fontSizeUp/index.js
+++ fontSizeUp/index.js
@@ -3,5 +3,9 @@
 // }
 
 document.getElementById('btn').addEventListener("click", function(){
-    document.getElementById('text').style.fontSize = (parseFloat(window.getComputedStyle(document.getElementById('text'), null).getPropertyValue('font-size')) + 5) + 'px';
+    document.getElementById('text').style.fontSize = (parseInt(window.getComputedStyle(document.getElementById('text'), null).getPropertyValue('font-size')) + 5) + 'px';
+})
+
+document.getElementById('btn2').addEventListener("click", function(){
+    document.getElementById('text').style.fontSize = (parseInt(window.getComputedStyle(document.getElementById('text'), null).getPropertyValue('font-size')) - 5) + 'px';
 })
