4ec546d8fe5f180b3a15ebdb211191aaa0364d45
xodnd0507 <24457545yong@gmail.com>
2024-04-22 11:58:38 +0900

    feat : Add width height btn

--- fontSizeUp/index.js
+++ fontSizeUp/index.js
@@ -5,3 +5,14 @@ document.getElementById('btn').addEventListener("click", function(){
 document.getElementById('btn2').addEventListener("click", function(){
     document.getElementById('text').style.fontSize = (parseInt(window.getComputedStyle(document.getElementById('text'), null).getPropertyValue('font-size')) - 5) + 'px';
 })
+
+document.getElementById('btn3').addEventListener('click', function(){
+    document.getElementById('div').style.width = (parseInt(window.getComputedStyle(document.getElementById('div'), null).getPropertyValue('width')) + 10) + 'px';
+})
+
+
+
+
+document.getElementById('btn4').addEventListener('click', function(){
+    document.getElementById('div').style.height = (parseInt(window.getComputedStyle(document.getElementById('div'), null).getPropertyValue('height')) + 10) + 'px';
+})
