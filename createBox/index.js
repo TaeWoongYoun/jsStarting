e91e7b4fadb0a3485c5fee43bee6f7ab09ec7d41
xodnd0507 <24457545yong@gmail.com>
2024-04-23 09:44:54 +0900

    test : Create box btn

---
+++ createBox/index.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', function(){
+    let newDiv = document.createElement('div');
+    document.getElementById('section').appendChild(newDiv);
+})
