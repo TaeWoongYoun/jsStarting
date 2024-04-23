42eda64549a94cdf9aa915b3a12232b2f1b002b1
xodnd0507 <24457545yong@gmail.com>
2024-04-23 11:14:24 +0900

    test : Add random color

--- createBox/index.js
+++ createBox/index.js
@@ -1,4 +1,6 @@
+
 document.getElementById('btn').addEventListener('click', function(){
-    let newDiv = document.createElement('div');
+    var newDiv = document.createElement('div');
+    newDiv.style.background = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.getElementById('section').appendChild(newDiv);
-})
+});
