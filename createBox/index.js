a2c1db32a5439944cce5df2bdb1179b73c4a0a71
xodnd0507 <24457545yong@gmail.com>
2024-04-24 19:19:52 +0900

    fix : Code fix

--- createBox/index.js
+++ createBox/index.js
@@ -1,5 +1,5 @@
 document.getElementById('btn').addEventListener('click', function(){
-    var newDiv = document.createElement('div');
+    let newDiv = document.createElement('div');
     newDiv.style.background = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.getElementById('section').appendChild(newDiv);
 });
