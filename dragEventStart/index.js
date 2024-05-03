4ac8f643dacd63e5d5ecc2063dc7803a54783d13
xodnd0507 <24457545yong@gmail.com>
2024-05-03 16:01:28 +0900

    test : Test box drop

--- dragEventStart/index.js
+++ dragEventStart/index.js
@@ -1,7 +1,18 @@
-// 드래그시 이미지 변경
-const img = document.createElement('img');
-    img.src = 'https://cdn-icons-png.flaticon.com/512/25/25231.png';
+// // 드래그시 이미지 변경
+// const img = document.createElement('img');
+//     img.src = 'https://cdn-icons-png.flaticon.com/512/25/25231.png';
 
-document.getElementById('dragBox').addEventListener('dragstart', (e) =>{
-    e.dataTransfer.setDragImage(img, 100, 100);
-}, false)
+// document.getElementById('dragBox').addEventListener('dragstart', (e) =>{
+//     e.dataTransfer.setDragImage(img, 100, 100);
+// }, false)
+
+var drop = document.getElementById('drop');
+
+drop.addEventListener('dragover', (e) =>{
+    e.preventDefault(); // 필수
+    e.dataTransfer.dropEffect = 'move';
+});
+
+drop.addEventListener('drop', (e) => {
+    alert('박스가 드래그 되었습니다.');
+});
