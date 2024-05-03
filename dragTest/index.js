17aa3a2271b379b6aebb2e0924fde0126aba2a51
xodnd0507 <24457545yong@gmail.com>
2024-05-03 14:30:14 +0900

    test : DragEnd

--- dragTest/index.js
+++ dragTest/index.js
@@ -23,20 +23,22 @@ const main = document.getElementById('main');
 // })
 
 //드롭 이벤트 테스트
-main.addEventListener('dragover', (e) =>{
-    e.preventDefault();
-})
+// main.addEventListener('dragover', (e) =>{
+//     e.preventDefault();
+// })
+// main.addEventListener('drop', (e) => {
+//     e.preventDefault();
+//     console.log('1번 박스에 이미지 드롭')
+// })
+// box.addEventListener('dragover', (e) =>{
+//     e.preventDefault();
+// })
+// box.addEventListener('drop', (e) => {
+//     e.preventDefault();
+//     console.log('2번 박스에 이미지 드롭')
+// })
 
-main.addEventListener('drop', (e) => {
-    e.preventDefault();
-    console.log('1번 박스에 이미지 드롭')
-})
-
-box.addEventListener('dragover', (e) =>{
-    e.preventDefault();
-})
-
-box.addEventListener('drop', (e) => {
-    e.preventDefault();
-    console.log('2번 박스에 이미지 드롭')
+// 드래그가 끝나면 발생하는 이벤트
+item.addEventListener('dragend' , (e) => {
+    console.log('드래그가 끝나면 나오는 이벤트')
 })
