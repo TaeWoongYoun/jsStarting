ef5e576cccc271d446b9eb20c29fa8903d2b9ce2
xodnd0507 <24457545yong@gmail.com>
2024-05-07 20:26:37 +0900

    test : update

--- dragEventStart/index.js
+++ dragEventStart/index.js
@@ -71,3 +71,18 @@
 //         {offset : Number.NEGATIVE_INFINITY},
 //     ).element;
 // }
+
+// 간단하게 구현하는 이미지 이동
+function dragEnter(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
