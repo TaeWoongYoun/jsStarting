13f5319255144613a12227a3d04e4e7245dec1fa
xodnd0507 <24457545yong@gmail.com>
2024-05-08 07:26:04 +0900

    test : Clear delete btn

--- Gacheon/index.js
+++ Gacheon/index.js
@@ -1,14 +1,14 @@
 let add = document.getElementById('make')
 let remove = document.getElementById('remove')
-let new_p = document.querySelector('.main')
+let main = document.querySelector('.main')
 
 add.addEventListener('click', e=>{
     e.preventDefault();
     let element = document.createElement('p');
     element.innerHTML = "새로운 텍스트 입니다."
-    new_p.appendChild(element);
+    main.appendChild(element);
 })
 
 remove.addEventListener('click', e=>{ 
-    new_p.removeChild(new_p.lastChild);
+    main.removeChild(new_p.lastChild);
 })
