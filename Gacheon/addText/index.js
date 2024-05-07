924a6689a968b8c60df31ff6319e44244f50608d
xodnd0507 <24457545yong@gmail.com>
2024-05-08 07:40:37 +0900

    docs : Update file

--- Gacheon/addText/index.js
+++ Gacheon/addText/index.js
@@ -1,14 +1,16 @@
 let add = document.getElementById('make')
 let remove = document.getElementById('remove')
 let main = document.querySelector('.main')
+let counter = 0;
 
 add.addEventListener('click', e=>{
-    e.preventDefault();
+    counter++;
     let element = document.createElement('p');
-    element.innerHTML = "새로운 텍스트 입니다."
+    element.innerHTML = "새로운 텍스트 입니다." + counter;
     main.appendChild(element);
 })
 
 remove.addEventListener('click', e=>{ 
-    main.removeChild(new_p.lastChild);
+    main.removeChild(main.lastChild);
+    counter--;
 })
