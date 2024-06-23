d0939f8b390c93f71b51270ab138196bab5f231b
xodnd0507 <24457545yong@gmail.com>
2024-06-23 10:03:59 +0900

    test : Delete unnecessary code

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,30 +1,28 @@
 $(document).ready(function(){
-    $(function(){
-        const background = document.querySelector('.container');
-        const colorText = document.querySelector('.color');
-        const btn = document.querySelector('button');
+    const background = document.querySelector('.container');
+    const colorText = document.querySelector('.color');
+    const btn = document.querySelector('button');
 
-        const colors = [
-            { id : 1, name : 'red'},
-            { id : 2, name : 'orange'},
-            { id : 3, name : 'yellow'},
-            { id : 4, name : 'lime'},
-            { id : 5, name : 'skyblue'},
-            { id : 6, name : 'aquamarine'},
-            { id : 7, name : 'coral'},
-            { id : 8, name : 'tomato'},
-            { id : 9, name : 'springreen'},
-            { id : 10, name : 'gray'},
-            { id : 11, name : 'pink'}
-        ]
+    const colors = [
+        { id : 1, name : 'red'},
+        { id : 2, name : 'orange'},
+        { id : 3, name : 'yellow'},
+        { id : 4, name : 'lime'},
+        { id : 5, name : 'skyblue'},
+        { id : 6, name : 'aquamarine'},
+        { id : 7, name : 'coral'},
+        { id : 8, name : 'tomato'},
+        { id : 9, name : 'springgreen'},
+        { id : 10, name : 'gray'},
+        { id : 11, name : 'pink'}
+    ]
 
-        const click = () => {
-            const random = Math.floor(Math.random() * colors.length);
-            background.style.backgroundColor = colors[random].name;
-            colorText.style.color = colors[random].name;
-            colorText.textContent = colors[random].name;
-        }
+    const click = () => {
+        const random = Math.floor(Math.random() * colors.length);
+        background.style.backgroundColor = colors[random].name;
+        colorText.style.color = colors[random].name;
+        colorText.textContent = colors[random].name;
+    }
 
-        btn.addEventListener('click', click);
-    })
+    btn.addEventListener('click', click);
 })
