0574a8a530eb319dc4e1594b290050b0025da6c3
xodnd0507 <24457545yong@gmail.com>
2024-06-24 09:38:16 +0900

    fix : Refactoring

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -1,13 +1,11 @@
-$(document).ready(function(){
-    const background = document.querySelector('.container');
-    const colorText = document.querySelector('.color');
-    const btn = document.querySelector('button');
-    const colors = ['red', 'orange', 'yellow', 'lime', 'skyblue', 'aquamarine', 'coral', 'tomato', 'springgreen', 'gray', 'pink']
+const background = document.querySelector('.container');
+const colorText = document.querySelector('.color');
+const btn = document.querySelector('button');
+const colors = ['red', 'orange', 'yellow', 'lime', 'skyblue', 'aquamarine', 'coral', 'tomato', 'springgreen', 'gray', 'pink']
 
-    btn.addEventListener('click', function(){
-        const random = Math.floor(Math.random() * colors.length);
-        background.style.backgroundColor = colors[random];
-        colorText.style.color = colors[random];
-        colorText.textContent = colors[random];
-    });
-})
+btn.addEventListener('click', function(){
+    const random = Math.floor(Math.random() * colors.length);
+    background.style.backgroundColor = colors[random];
+    colorText.style.color = colors[random];
+    colorText.textContent = colors[random];
+});
