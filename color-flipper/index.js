5af9dfcf208441664a44c3d40eaa8a2098e434f9
xodnd0507 <24457545yong@gmail.com>
2024-07-10 17:13:00 +0900

    docs : Change text

--- color-flipper/index.js
+++ color-flipper/index.js
@@ -3,9 +3,9 @@ const colorText = document.querySelector('.color');
 const btn = document.querySelector('button');
 const colors = ['red', 'orange', 'yellow', 'lime', 'skyblue', 'aquamarine', 'coral', 'tomato', 'springgreen', 'gray', 'pink']
 
-    btn.addEventListener('click', () => {
-    const random = Math.floor(Math.random() * colors.length);
-    background.style.backgroundColor = colors[random];
-    colorText.style.color = colors[random];
-    colorText.textContent = colors[random];
+btn.addEventListener('click', () => {
+const random = Math.floor(Math.random() * colors.length);
+background.style.backgroundColor = colors[random];
+colorText.style.color = colors[random];
+colorText.textContent = colors[random];
 });
